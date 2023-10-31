d96851d751c5ca0fcc1b3854eeccc7113352a26b
sothh0117 <ljss1211@gmail.com>
2023-11-01 01:22:45 +0900

    캐릭터 공격 및 hp제어
    hp제어쪽 아직 미완성
    및 enemy 스크립트 조정 필요

---
+++ Assets/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject prfHpBar;
+    public GameObject canvas;
+
+    public string enemyName;
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public int atkSpeed;
+
+    private void SetEnemyStatus(string _enemyName, int _maxHp, int _atkDmg, int _atkSpeed)
+    {
+        enemyName = _enemyName;
+        maxHp = _maxHp;
+        nowHp = _maxHp;
+        atkDmg = _atkDmg;
+        atkSpeed = _atkSpeed;
+    }
+
+    public main_character sword_man;
+    Image nowHpbar;
+
+    RectTransform hpBar;
+
+    public float height = 1.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
+        if(name.Equals("Enemy1"))
+        {
+            SetEnemyStatus("Enemy1", 100, 10, 1);
+        }
+        nowHpbar = hpBar.transform.GetChild(0).GetComponent(Image)();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
+        hpBar.position = _hpBarPos;
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            nowHp -= sword_man.atkDmg;
+            Debug.Log(nowHp);
+            sword_man.attacked = false;
+            if (nowHp <= 0)
+            {
+                Destroy(gameObject);
+                Destroy(hpBar.gameObject);
+            }
+        }
+    }
+}
--- Assets/main_character.cs
+++ Assets/main_character.cs
@@ -1,23 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class main_character : MonoBehaviour
 {
     public GameObject character;
     Animator animator;
 
+    public int maxHp;
+    public int nowHp;
+    public int atkDmg;
+    public float atkSpeed = 1;
+    public bool attacked = false;
+    public Image nowHpbar;
+
+    void AttackTrue()
+    {
+        attacked = true;
+    }
+    void AttackFalse()
+    {
+        attacked = false;
+    }
+    void SetAttackSpeed(float speed)
+    {
+        animator.SetFloat("attackSpeed", speed);
+        atkSpeed = speed;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        maxHp = 50;
+        nowHp = 50;
+        atkDmg = 10;
+
         character.transform.position = new Vector3(0,0,0);
         animator = GetComponent<Animator>();
+        SetAttackSpeed(1.5f);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        nowHpbar.fillAmount = (float)nowHp / (float)maxHp;
         float h = Input.GetAxis("Horizontal");
         if (h > 0)
         {
